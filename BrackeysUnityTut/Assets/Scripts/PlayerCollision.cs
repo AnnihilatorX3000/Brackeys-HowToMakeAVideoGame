bb8380ac46f4d784cedcd41bf0d951f116b573c6
Calvin James Newn <calvin.newn@mail.mcgill.ca>
2019-12-30 21:02:43 -0500

    Initial commit

---
+++ BrackeysUnityTut/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movementScript;   // Referencing another script
+
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("Ow");
+            movementScript.enabled = false;     // Disables player movement on collision
+        }
+    }
+}
---
+++ BrackeysUnityTut/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;    //For referencing components of Player gameobject
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game Initialised");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    void FixedUpdate()
+    {
+        //Always moving forward
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        //Player movement - not the most efficient way
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
